820562fa0d4d1705eb44411901c6b89f6474285f
leonardo camargo <leonardo.camargo.contato@gmail.com>
2022-07-20 00:07:27 -0300

    Testes para deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express();
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if (err) {
+        return console.log(err)
+    }
+
+    console.log('Foi');
+}) 
